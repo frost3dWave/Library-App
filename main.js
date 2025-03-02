24379f77b3a04ae5cb0620e237186715dd8b600e
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2025-03-02 15:43:02 +0530

    updated to populate the table with html form field
    additionally made form field reset each time a entry is submitted, to facilitate adding new entries quicker & more intuitive.
    added comments

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
+const openBtn = document.querySelector(".open-modal");
+const dialog = document.querySelector(".dialog");
+const closeBtn = document.querySelector(".close-btn");
+const submitBtn = document.querySelector(".submitBtn");
+
+const bookTitle = document.querySelector("#book-title");
+const bookAuthor = document.querySelector("#book-author");
+const bookPages = document.querySelector("#book-pages");
+const bookStatus = document.querySelector("#status");
+
 const tbody = document.querySelector(".table-body");
 
 const myLibrary = [
-    {
-        title: "newBook",
-        author: "newAuthor",
-        pages: 100,
-        status: "not read",
+    // {
+    //     title: "newBook",
+    //     author: "newAuthor",
+    //     pages: 100,
+    //     status: "not read",
 
-        info: function(){
-            return `"${this.title} by ${this.author}, ${this.pages} pages, ${this.status}"`
-        }
-    },
-    {
-        title: "newBook2",
-        author: "newAuthor2",
-        pages: 200,
-        status: "completed",
+    //     info: function(){
+    //         return `"${this.title} by ${this.author}, ${this.pages} pages, ${this.status}"`
+    //     }
+    // },
+    // {
+    //     title: "newBook2",
+    //     author: "newAuthor2",
+    //     pages: 200,
+    //     status: "completed",
 
-        info: function(){
-            return `"${this.title} by ${this.author}, ${this.pages} pages, ${this.status}"`
-        }
-    },
-    {
-        title: "newBook3",
-        author: "newAuthor3",
-        pages: 300,
-        status: "in progess",
+    //     info: function(){
+    //         return `"${this.title} by ${this.author}, ${this.pages} pages, ${this.status}"`
+    //     }
+    // },
+    // {
+    //     title: "newBook3",
+    //     author: "newAuthor3",
+    //     pages: 300,
+    //     status: "in progess",
 
-        info: function(){
-            return `"${this.title} by ${this.author}, ${this.pages} pages, ${this.status}"`
-        }
-    },
+    //     info: function(){
+    //         return `"${this.title} by ${this.author}, ${this.pages} pages, ${this.status}"`
+    //     }
+    // },
 ];
 
 function Book(title, author, pages, status) {
@@ -45,25 +55,45 @@ function Book(title, author, pages, status) {
 };
 
 function addBookToLibrary(title, author, pages, status) {
-    const book1 = new Book(title, author, pages, status);
-
-    myLibrary.push(book1);
+    myLibrary.push(new Book(title, author, pages, status));
 };
 
 function showBooks(array) {
-    array.map(book => {
-        let row = tbody.insertRow();
-        let title = row.insertCell(0);
-        title.textContent = book.title;
+    array.forEach(book => {
+        let row = tbody.insertRow();  // insert row on tbody tag to get the data entries
+
+        let title = row.insertCell(0);   // add first cell in the row & similar below
+        title.innerHTML = book.title;  
+
         let author = row.insertCell(1);
-        author.textContent = book.author;
+        author.innerHTML = book.author;
+
         let pages = row.insertCell(2);
-        pages.textContent = book.pages;
+        pages.innerHTML = book.pages;
+
         let status = row.insertCell(3);
-        status.textContent = book.status;
+        status.innerHTML = book.status;
+
         let actions = row.insertCell(4);
-        actions.textContent = "DELETE";
+        actions.innerHTML = "DELETE";
     })
 };
 
-showBooks(myLibrary);
+openBtn.addEventListener("click", () => dialog.showModal());
+
+closeBtn.addEventListener("click", () => dialog.close());
+
+submitBtn.addEventListener("click", event => {
+    event.preventDefault();  // prevent default submit behavior of sending data to the server
+
+    tbody.innerHTML = "";    // remove all the current html child elements from tbody element to not get repeating data entries of earlier rows each time a new data is added.
+
+    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value);
+    showBooks(myLibrary);
+
+    // reset the form to start with fresh form fields for new entries
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    bookPages.value = "";
+    bookStatus.value = "not-started";
+});
